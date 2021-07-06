0596514d3f55c6159ab46a4d28eefce872104b0a
Aleks Kudryashov <aleks59niki30@gmail.com>
2021-07-06 07:06:49 +0300

    add bloodtest meta

--- bloodtest/mainapp/models.py
+++ bloodtest/mainapp/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class BloodTest(models.Model):
     personal_number = models.PositiveIntegerField()
     datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.personal_number}'
+
+    class Meta:
+        verbose_name = 'Blood Test'
+        verbose_name_plural = 'Blood Tests'
+        ordering = ['-datetime']
