826d6b70bbd5f18e428e5688f91937b812fdc8e3
Aleks Kudryashov <aleks59niki30@gmail.com>
2021-07-06 10:32:06 +0300

    edit bloodtest model

--- bloodtest/mainapp/admin.py
+++ bloodtest/mainapp/admin.py
@@ -5,8 +5,8 @@ from .models import *
 
 @admin.register(BloodTest)
 class BloodTestAdmin(admin.ModelAdmin):
-    list_display = ('personal_number', 'datetime')
+    list_display = ('personal_number', 'name', 'surname', 'patronymic', 'birthday', 'datetime')
     list_display_links = ('personal_number',)
 
     ordering = ['-datetime']
-    search_fields = ['personal_number']
+    search_fields = ['personal_number', 'name', 'surname', 'patronymic']
--- bloodtest/mainapp/models.py
+++ bloodtest/mainapp/models.py
@@ -3,10 +3,16 @@ from django.db import models
 
 class BloodTest(models.Model):
     personal_number = models.PositiveIntegerField()
+
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+    patronymic = models.CharField(max_length=32, blank=True, null=True)
+
+    birthday = models.DateField()
     datetime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.personal_number}'
+        return f'{self.personal_number}-{self.name}{self.surname}'
 
     class Meta:
         verbose_name = 'Blood Test'
