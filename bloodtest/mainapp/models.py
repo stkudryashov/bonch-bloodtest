23c8e0ab4f96ea19d9ae8799c8dcc5a8c8e9b129
Gleb <37934662+quanuhs@users.noreply.github.com>
2021-07-07 19:33:17 +0300

    Update

--- bloodtest/mainapp/models.py
+++ bloodtest/mainapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid
 
 
 class BloodTest(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     personal_number = models.PositiveIntegerField()
 
     name = models.CharField(max_length=32)
@@ -18,3 +20,28 @@ class BloodTest(models.Model):
         verbose_name = 'Blood Test'
         verbose_name_plural = 'Blood Tests'
         ordering = ['-datetime']
+
+
+class Analysis(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(BloodTest, null=False, on_delete=models.CASCADE, related_name="user")
+    name = models.CharField(null=False, max_length=64)
+    datetime = models.DateField(null=False)
+
+    def __str__(self):
+        return f'{self.name} ({self.datetime}) - {self.user}'
+
+    class Meta:
+        verbose_name = 'Analysis'
+        verbose_name_plural = 'Analysis'
+        ordering = ['-datetime']
+
+
+class Indicator(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(null=False, max_length=64)
+    value = models.FloatField(null=False)
+    analysis = models.ForeignKey(Analysis, on_delete=models.CASCADE, related_name="analysis")
+
+    def __str__(self):
+        return f'{self.name} - {self.analysis}'
